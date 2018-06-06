f66ab7f37748da55e3e0c747b2403c5cd8c384f0
Messy <ronildofacanhadacunhajunior@gmail.com>
2018-06-06 01:36:56 -0400

    upDate

--- Assets/Scripts/Inimigos.cs
+++ Assets/Scripts/Inimigos.cs
@@ -8,12 +8,24 @@ public class Inimigos : MonoBehaviour {
     private float MaxVida;
     public Text textVida;
     public Slider sliderVida;
+    public Rigidbody m_Rigidbody;
+
+
+    private void Start()
+    {
+        m_Rigidbody.GetComponent<Rigidbody>();    
+    }
 
     private void Awake()
     {
         sliderVida.value = vida;
         MaxVida = vida;
         textVida.text = MaxVida + "/" + vida.ToString();
+    }
+
+    private void Update()
+    {
+        m_Rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
     }
 
     private void OnTriggerEnter(Collider other)
